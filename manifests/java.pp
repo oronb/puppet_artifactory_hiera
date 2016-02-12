daca8c91a8f8a76246f70794511e0b2ac6fcea98
oronb <orong1234@gmail.com>
2016-02-13 01:03:15 +0200

    Update java.pp

--- manifests/java.pp
+++ manifests/java.pp
@@ -1,13 +1,10 @@
 #java.pp
 class artifactory::java (
-#$java_version         = artifactory::params::java_version,
 $package_java_name    = $artifactory::params::package_java_name
 ) inherits artifactory::params {
    package { "${package_java_name}":
-   #provider => $repo_type,
+   provider => $repo_type,
    ensure   => 'installed',
-   #source   => "/tmp/${package_name}-${version}.${repo_type}",
-   #require  => Exec["download ${package_name}-${version}"]
     }
 }
 
