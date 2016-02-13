9b1faa8896a2e49aabda821efb1edb950cd6304a
oronb <orong1234@gmail.com>
2016-02-13 16:06:40 +0200

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,14 +6,14 @@ class artifactory::params {
 	$java_version                = hiera('artifactory::params::java_version', '8')
 	$source                      = "/tmp"
 	$destination                 = "/opt/jfrog/artifactory"
-	$package_artifactory_ensure  = "installed"
+	$ensure                       = "installed"
 	$ajp_port                    = hiera('artifactory::params:ajp_port', '8019')
 
         if ($version =~ /^2/) or ($version =~ /^3/) {    
-           $artifactory_type = "undef"
+           $type = "undef"
         }
 	else {
-   	   $artifactory_type = hiera('artifactory::params::artifactory_type', 'oss') 
+   	   $type = hiera('artifactory::params::artifactory_type', 'oss') 
         }
 
 	case $java_version {
@@ -34,12 +34,12 @@ class artifactory::params {
 	     'Debian': {   $repo_type     = "deb"
 		           $repo_provider = "dpkg"
                		   $package_java_name  = "openjdk-${java_version}-jdk"
-                    if $artifactory_type == "pro" {
+                    if $type == "pro" {
                            $package_artifactory_name    = "jfrog-artifactory-${artifactory_type}"
                            $path                        = "pool/main/j/jfrog-artifactory-${artifactory_type}-${repo_type}"
                            $repo_source                 = "http://jfrog.bintray.com/artifactory-${artifactory_type}-${repo_type}s/${path}/jfrog-artifactory-${artifactory_type}-${version}.${repo_type}"
                     }
-                    if $artifactory_type == "oss" {
+                    if $type == "oss" {
                            $package_artifactory_name    = "jfrog-artifactory-${artifactory_type}"
                            $path                        = "pool/main/j/jfrog-artifactory-${artifactory_type}-${repo_type}"
                            $repo_source                 = "http://jfrog.bintray.com/artifactory-${repo_type}s/${path}/jfrog-artifactory-${artifactory_type}-${version}.${repo_type}"
@@ -48,18 +48,18 @@ class artifactory::params {
 	     'Redhat': { 
 	                   $repo_type           = "rpm" 
 		           $repo_provider       = "rpm" 
-			   $package_java_name   = "java-1.${java_version}.0-openjdk"
+			   $package_java        = "java-1.${java_version}.0-openjdk"
 	                 if $artifactory_type == "pro" {
-	                   $package_artifactory_name    = "jfrog-artifactory-${artifactory_type}"
+	                   $package    = "jfrog-artifactory-${artifactory_type}"
 	                   $path                        = "org/artifactory/${artifactory_type}/${repo_type}/jfrog-artifactory-${repo_type}/${version}"
 	                   $repo_source                 = "http://jfrog.bintray.com/artifactory-${artifactory_type}-${repo_type}s/$path/jfrog-artifactory${artifactory_type}-${version}.${repo_type}"
 	                 }
 	                 if  $artifactory_type == "oss" {
-	                   $package_artifactory_name    = "jfrog-artifactory-${artifactory_type}"
+	                   $package    = "jfrog-artifactory-${artifactory_type}"
 	                   $repo_source                 = "http://jfrog.bintray.com/artifactory-${repo_type}s/jfrog-artifactory-${artifactory_type}-${version}.${repo_type}"
 	                 }
 	                 if  $artifactory_type == "unef" { 
-	                   $package_artifactory_name    = "artifactory"
+	                   $package                     = "artifactory"
 	                   $repo_source                 = "http://jfrog.bintray.com/artifactory-${repo_type}s/artifactory-${version}.${repo_type}"
 	                }
 	     }
